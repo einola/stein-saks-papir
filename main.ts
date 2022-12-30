c2c3f81b058837c516beb817a5ec3a5262952c7a
Einar Örn Ólason <einar.olason@nersc.no>
2022-12-30 10:12:40 +0000

    The project didn't load from github - so this is a rewrite(!)

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ input.onGesture(Gesture.Shake, function () {
     basic.showNumber(1)
     basic.showNumber(2)
     basic.showNumber(3)
-    Rock_paper_scissors = randint(1, 5)
+    Rock_paper_scissors = randint(0, 5)
     if (Rock_paper_scissors == 1) {
         basic.showLeds(`
             . # # # .
